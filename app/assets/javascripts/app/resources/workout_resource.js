ad0bc0f588248864be9d32a31e164ddff7cda749
Elmano Neto <elmanoneto@gmail.com>
2016-03-13 22:47:43 -0300

    clean project

--- app/assets/javascripts/app/resources/workout_resource.js
+++
@@ -1,4 +0,0 @@
-angular.module('workout.services', [])
-  .factory('WorkoutService', function ($resource) {
-      return $resource('/workouts/:id', {'query': {method: 'GET', isArray: false}});
-  });
